c13e21978d78a8ba7c7a3b5cc3828d7f31f5e5e4
nikhil3113 <chavann717@gmail.com>
2025-11-23 21:30:54 +0530

    added ga tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Appbar } from "@/components/Appbar";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Appbar />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-10NYWZM6Q4" />
     </html>
   );
 }
